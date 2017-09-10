1a8700955e27bdc6478596b1e06188e1878caf8a
Vishwa Krishnakumar <vishwa306@gmail.com>
2017-09-09 18:11:57 -0700

    Connected to Watson API. Added intents

--- server/watsonify.js
+++ server/watsonify.js
@@ -15,7 +15,7 @@ var fs = require('fs');
 var obj = JSON.parse(fs.readFileSync(path.join(__dirname, '/../common/models/'+model_name+".json"), "utf8"));
 
 console.log(obj);
-
+/*
 var watson = require("watson-developer-cloud");
 
 var conversation = new watson.ConversationV1({
@@ -32,6 +32,53 @@ var params = {
 };
 
 conversation.createEntity(params, function(err, response) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(JSON.stringify(response, null, 2));
+  }
+
+});
+*/
+
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+}
+
+var watson = require("watson-developer-cloud");
+
+var conversation = new watson.ConversationV1({
+  username: process.env.npm_config_username,
+  password: process.env.npm_config_password,
+  version_date: '2017-09-08'
+});
+
+if(model_name.indexOf("_") != -1){  
+  var examples = [
+    {
+      text: model_name,
+    },
+    {
+      text: replaceAll(model_name, "_", " ")
+    }
+  ];
+}
+else{
+  var examples = [
+    {
+      text: model_name,
+    }
+  ];
+
+}
+
+var params = {
+  workspace_id: 'ad7e5d93-86a0-4372-a6b2-a86ed7e31234',
+  intent: model_name,
+  examples: examples
+};
+
+conversation.createIntent(params, function(err, response) {
   if (err) {
     console.error(err);
   } else {
