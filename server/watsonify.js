bbe1cf46d9883a03fe852665e6062ad779687215
Vishwa Krishnakumar <vishwa306@gmail.com>
2017-09-09 17:32:50 -0700

    Bug fixed in watsonify.js

--- server/watsonify.js
+++ server/watsonify.js
@@ -1,3 +1,9 @@
 'use strict';
 
 //impliment Watson conversation API code here
+if process.env.npm_config_model{
+  model_name = process.env.npm_config_model
+}
+else{
+  throw new Error("Enter 'model' parameter");
+}
